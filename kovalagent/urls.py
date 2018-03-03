df912d9a4033e1cb1931885054fe5aa35927cd30
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-03 22:33:22 +0200

    Add url to login

--- kovalagent/urls.py
+++ kovalagent/urls.py
@@ -1,9 +1,13 @@
 """KovalAgent URL Configuration."""
 
+from django.conf.urls.static import static
 from django.conf.urls import url, include
+from django.conf import settings
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'', include('main.urls')),
+    url(r'^accounts/login/$', views.login, name='login'),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
