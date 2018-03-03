718ab96173d04b23a037dec5e1ae4d11f2bc47de
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-03 21:11:23 +0200

    Add view for iteam details

--- main/views.py
+++ main/views.py
@@ -9,3 +9,9 @@ def equipment(request):
     """Render page with equipment list."""
     iteams = Equipment.objects.all().order_by('status')
     return render(request, 'main/equipment.html', {'iteams': iteams})
+
+
+def iteam_details(request, pk):
+    """Render page with iteam details."""
+    iteam = Equipment.objects.get(id=pk)
+    return render(request, 'main/iteam_details.html', {'iteam': iteam})
