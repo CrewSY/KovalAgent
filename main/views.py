e61e7a9c06bbb30b769028f2105d7ebca8d8b38a
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-03 21:00:32 +0200

    Add view for main page

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 """Views for main app of PowerSocket project."""
 
-from django.shortcuts import render  # noqa: F401
+from django.shortcuts import render
+
+from .models import Equipment
+
+
+def equipment(request):
+    """Render page with equipment list."""
+    iteams = Equipment.objects.all().order_by('status')
+    return render(request, 'main/equipment.html', {'iteams': iteams})
