70100613d5901583ae5ec4b3a1edec691b497beb
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-03 20:28:50 +0200

    Configure equipment model on admin page

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,21 @@
 """Management of main app admin panel."""
 
-from django.contrib import admin  # noqa: F401
+from django.contrib import admin
+
+from .models import Equipment
+
+
+class EquipmentAdmin(admin.ModelAdmin):
+    """Config equipment on admin page."""
+
+    list_display = [field.name for field in Equipment._meta.fields]
+    list_filter = ('status', )
+    search_fields = ['title', ]
+
+    class Meta:
+        """Meta data of EquipmentAdmin."""
+
+        model = Equipment
+
+
+admin.site.register(Equipment, EquipmentAdmin)
