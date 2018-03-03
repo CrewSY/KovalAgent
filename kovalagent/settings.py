da3ac84a37333dc91de1ac349beb0f37006eab2a
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-03 22:30:25 +0200

    Configure django registrations redux

--- kovalagent/settings.py
+++ kovalagent/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'registration',
 ]
 
 MIDDLEWARE = [
@@ -109,3 +110,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
+REGISTRATION_AUTO_LOGIN = True
