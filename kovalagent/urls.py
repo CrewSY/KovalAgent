988ead45804ff1b8f60ef20f3cb75868aec00fbb
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-03 20:50:11 +0200

    Include main urls

--- kovalagent/urls.py
+++ kovalagent/urls.py
@@ -1,7 +1,9 @@
 """KovalAgent URL Configuration."""
-from django.conf.urls import url
+
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'', include('main.urls')),
     url(r'^admin/', admin.site.urls),
 ]
