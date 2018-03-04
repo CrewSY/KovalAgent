03193c6c88303d13c36e694bf3c269ef3e4e598b
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-04 15:23:54 +0200

    Add url to logs

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^$', views.equipment, name='equipment'),
     url(r'^iteam_details/(?P<pk>[0-9]+)/$', views.iteam_details, name='iteam_details'),
     url(r'^change_status/$', views.change_status, name='change_status'),
+    url(r'^logs/$', views.logs, name='logs'),
 ]
