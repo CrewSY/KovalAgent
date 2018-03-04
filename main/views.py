688dc1ab9c0a5257df80d124d03f82b046503d50
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-04 15:04:05 +0200

    Update view for changing status

--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
 """Views for main app of PowerSocket project."""
 
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Equipment, EquipmentLog
+from .models import Equipment, EquipmentLog, Status
 
 
 def equipment(request):
@@ -18,21 +19,24 @@ def iteam_details(request, pk):
     return render(request, 'main/iteam_details.html', {'iteam': iteam})
 
 
-def create_log(request):
-    """Render page with equipment list."""
+def change_status(request):
+    """Change status and create log object for this action."""
     user = request.user
     data = request.POST
 
     if user.is_authenticated():
         iteam_id = data.get('iteam_id')
-        iteam = Equipment.objects.filer(id=iteam_id)
-        pre_status = iteam.status
+        post_status_id = data.get('post_status_id')
 
-        post_status = data.get('post_status')
+        iteam = Equipment.objects.get(id=iteam_id)
+        post_status = Status.objects.get(id=post_status_id)
+        pre_status = iteam.status.name
+
         EquipmentLog.objects.create(owner=user,
-                                    iteam=iteam_id,
+                                    iteam=iteam,
                                     pre_status=pre_status,
                                     post_status=post_status)
-        iteam.update(status=post_status)
+        iteam.status = post_status
+        iteam.save()
 
     return HttpResponse()
