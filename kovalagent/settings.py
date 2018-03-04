a132e67c5ceb1b4ce674c2f9e17678753e18d1ea
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-04 21:06:31 +0200

    Fix repository root

--- kovalagent/settings.py
+++ kovalagent/settings.py
@@ -5,6 +5,7 @@ import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+REPOSITORY_ROOT = os.path.dirname(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,7 +122,7 @@ LOGIN_REDIRECT_URL = '/'
 REGISTRATION_AUTO_LOGIN = True
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
+MEDIA_ROOT = os.path.join(REPOSITORY_ROOT, 'media/')
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
