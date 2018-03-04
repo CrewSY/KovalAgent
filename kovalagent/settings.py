3d198ea34e19f423abf4503726c97bd3ce940885
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-04 20:07:15 +0200

    Configure heroku deployment

--- kovalagent/settings.py
+++ kovalagent/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3-!x1e-yd75exuvrgcs7r&pxj7jgi15v0=^wj9a0k@h6#!mdb!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
