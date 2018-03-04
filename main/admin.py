370d71adc83f94eaccb1605902b64c2c82489c61
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-04 10:01:23 +0200

    Fix admin page config

--- main/admin.py
+++ main/admin.py
@@ -8,7 +8,11 @@ from .models import Equipment, EquipmentLog
 class EquipmentAdmin(admin.ModelAdmin):
     """Config equipment on admin page."""
 
-    list_display = [field.name for field in Equipment._meta.fields]
+    list_display = ('id',
+                    'title',
+                    'status',
+                    'description',
+                    )
     list_filter = ('status', )
     search_fields = ['title', ]
 
@@ -24,12 +28,18 @@ admin.site.register(Equipment, EquipmentAdmin)
 class EquipmentLogAdmin(admin.ModelAdmin):
     """Config equipment log on admin page."""
 
-    list_display = [field.name for field in Equipment._meta.fields]
+    list_display = ('owner',
+                    'iteam',
+                    'pre_status',
+                    'post_status',
+                    'date',
+                    )
     list_filter = ('owner', 'pre_status', 'post_status', )
-    search_fields = ['owner',
+    search_fields = ('owner',
                      'owner.user__first_name',
                      'owner.user__last_name',
-                     'iteam__title', ]
+                     'iteam.title',
+                     )
 
     class Meta:
         """Meta data of EquipmentLogAdmin."""
