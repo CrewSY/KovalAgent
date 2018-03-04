16a078dcacb516fa79f78b01f85c2b6f30340a45
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-04 19:01:46 +0200

    Add url for logout

--- kovalagent/urls.py
+++ kovalagent/urls.py
@@ -9,5 +9,6 @@ from django.contrib.auth import views
 urlpatterns = [
     url(r'', include('main.urls')),
     url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
