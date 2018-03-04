c0fbb2993e91739ecc7d91b4872bc23e6b811910
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-04 15:47:32 +0200

    Add beforesend and success

--- main/static/js/iteam-details.js
+++ main/static/js/iteam-details.js
@@ -6,7 +6,6 @@ function initStatusButtons(){
         var btns = $('#btns-status');
         var iteam_id= btns.data("iteam_id");
         var url = btns.data("url");
-        console.log(url, iteam_id, post_status_id)
 
         statusUpdating(iteam_id, url, post_status_id);
     });
@@ -21,28 +20,58 @@ function statusUpdating(iteam_id, url, post_status_id){
     data.iteam_id = iteam_id;
     data.post_status_id = post_status_id;
 
+    var indicator = $('#ajax-progress-indicator');
+
     $.ajax({
         url: url,
         type: 'POST',
         data: data,
         cache: true,
+        'beforeSend': function(xhr, settings){
+            indicator.show();
+        },
+        'success': function(data, status, xhr){
+            indicator.hide();
+            location.reload();
+        }
     });
 }
-
-
-$(document).ready(function(){
-    initStatusButtons();
-    // changeIcons();
-});
-
-
-
 
 
 function changeIcons() {
   $('.change-icon').click(function(){
     var button = $(this);
     button.prop("disabled", "disabled");
-    var icon = button.find('i').removeClass('fa-cart-plus').addClass('fa-check-circle');
   });
 }
+
+function initDisabledButton(){
+    var id = $('#iteam-status').data("status_id");
+    var btn1 = $('#btn1');
+    var btnid1 = btn1.data("post_status_id");
+    if(btnid1==id){
+        btn1.prop("disabled", "disabled");
+    }
+    var btn2 = $('#btn2');
+    var btnid2 = btn2.data("post_status_id");
+    if(btnid2==id){
+        btn2.prop("disabled", "disabled");
+    }
+    var btn3 = $('#btn3');
+    var btnid3 = btn3.data("post_status_id");
+    if(btnid3==id){
+        btn3.prop("disabled", "disabled");
+    }
+    var btn4 = $('#btn4');
+    var btnid4 = btn4.data("post_status_id");
+    if(btnid4==id){
+        btn4.prop("disabled", "disabled");
+    }
+}
+
+
+$(document).ready(function(){
+    initStatusButtons();
+    changeIcons();
+    initDisabledButton();
+});
